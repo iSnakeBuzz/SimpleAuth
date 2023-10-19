7c93a6e38cd4ddc441a38ab8cb10d5c0e9ea7898
Martín R. O <contacto@isnakebuzz.com>
2023-10-19 16:52:38 -0300

    Working on config

---
+++ src/main/java/rip/snake/simpleauth/utils/ConfigCreator.java
@@ -0,0 +1,41 @@
+package rip.snake.simpleauth.utils;
+
+import dev.dejvokep.boostedyaml.YamlDocument;
+import dev.dejvokep.boostedyaml.dvs.versioning.BasicVersioning;
+import dev.dejvokep.boostedyaml.settings.dumper.DumperSettings;
+import dev.dejvokep.boostedyaml.settings.general.GeneralSettings;
+import dev.dejvokep.boostedyaml.settings.loader.LoaderSettings;
+import dev.dejvokep.boostedyaml.settings.updater.UpdaterSettings;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.util.Objects;
+
+public class ConfigCreator {
+
+    private final Path pluginFolder;
+
+    private YamlDocument config;
+
+    public ConfigCreator(Path pluginFolder) {
+        this.pluginFolder = pluginFolder;
+    }
+
+    public void createConfig() throws IOException {
+        try (InputStream resourceAsStream = getClass().getResourceAsStream("/config.yml")) {
+            config = YamlDocument.create(
+                    pluginFolder.resolve("config.yml").toFile(),
+                    Objects.requireNonNull(resourceAsStream, "Could not find config.yml in the jar!"),
+                    GeneralSettings.DEFAULT,
+                    LoaderSettings.builder().setAutoUpdate(true).build(),
+                    DumperSettings.DEFAULT,
+                    UpdaterSettings.builder().setVersioning(new BasicVersioning("file-version")).setOptionSorting(UpdaterSettings.OptionSorting.SORT_BY_DEFAULTS).build()
+            );
+
+            config.update();
+            config.save();
+        }
+    }
+
+}
