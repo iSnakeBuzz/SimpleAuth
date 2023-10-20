b2cfe4d4d22c3348062fdc1176c6ae95750c9001
Martín R. O <contacto@isnakebuzz.com>
2023-10-20 01:49:16 -0300

    Created a temporary player

---
+++ src/main/java/rip/snake/simpleauth/player/TPlayer.java
@@ -0,0 +1,30 @@
+package rip.snake.simpleauth.player;
+
+import com.velocitypowered.api.proxy.server.RegisteredServer;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class TPlayer {
+
+    private final String username;
+
+    private boolean needAuth;
+    private boolean loggedIn;
+
+    private RegisteredServer server;
+
+    public void needsAuth() {
+        this.needAuth = true;
+    }
+
+    public void loggedIn() {
+        this.loggedIn = true;
+    }
+
+    public void setServer(RegisteredServer server) {
+        this.server = server;
+    }
+
+}
