02b4cbc17492fdf01d366859bdf2475eb7855754
Martín R. O <contacto@isnakebuzz.com>
2023-10-20 01:49:35 -0300

    Created AuthPlayer

---
+++ src/main/java/rip/snake/simpleauth/player/AuthPlayer.java
@@ -0,0 +1,26 @@
+package rip.snake.simpleauth.player;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@Data
+public class AuthPlayer {
+
+    private final String uniqueId;
+    private final String username;
+
+    private final String last_server;
+    private final String last_ip;
+    private final long last_login;
+
+    private String hashed_password;
+    private boolean isPremium;
+
+    public UUID getUniqueId() {
+        return UUID.fromString(uniqueId);
+    }
+
+}
