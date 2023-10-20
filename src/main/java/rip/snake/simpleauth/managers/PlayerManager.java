5ae311800f8e3aec94b2e00085b3e3227780c4b5
Martín R. O <contacto@isnakebuzz.com>
2023-10-20 01:50:55 -0300

    Created player manager

---
+++ src/main/java/rip/snake/simpleauth/managers/PlayerManager.java
@@ -0,0 +1,34 @@
+package rip.snake.simpleauth.managers;
+
+import com.google.common.collect.Maps;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import rip.snake.simpleauth.player.AuthPlayer;
+import rip.snake.simpleauth.player.TPlayer;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class PlayerManager {
+
+    private static final Map<UUID, AuthPlayer> AUTH_PLAYERS = Maps.newConcurrentMap();
+    private static final Map<String, TPlayer> TMP_PLAYERS = Maps.newConcurrentMap();
+
+    public static void PUT_PLAYER(UUID uniqueId, AuthPlayer authPlayer) {
+        AUTH_PLAYERS.put(uniqueId, authPlayer);
+    }
+
+    public static void REMOVE_PLAYER(UUID uniqueId, String username) {
+        AUTH_PLAYERS.remove(uniqueId);
+        TMP_PLAYERS.remove(username);
+    }
+
+    @Nullable
+    public static AuthPlayer GET_PLAYER(UUID uniqueId) {
+        return AUTH_PLAYERS.getOrDefault(uniqueId, null);
+    }
+
+    public static TPlayer GET_TMP_PLAYER(String username) {
+        return TMP_PLAYERS.computeIfAbsent(username, s -> new TPlayer(username, false, false, null));
+    }
+
+}
