0ccea2a24d7dde343fca1ca146cc555a886a3b02
Martín R. O <contacto@isnakebuzz.com>
2023-10-20 01:51:51 -0300

    Created server listener

---
+++ src/main/java/rip/snake/simpleauth/listeners/ServerListener.java
@@ -0,0 +1,67 @@
+package rip.snake.simpleauth.listeners;
+
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.player.PlayerChooseInitialServerEvent;
+import com.velocitypowered.api.event.player.ServerPreConnectEvent;
+import com.velocitypowered.api.proxy.server.RegisteredServer;
+import lombok.AllArgsConstructor;
+import rip.snake.simpleauth.SimpleAuth;
+import rip.snake.simpleauth.managers.PlayerManager;
+import rip.snake.simpleauth.player.TPlayer;
+
+import java.util.Optional;
+
+@AllArgsConstructor
+public class ServerListener {
+    private final SimpleAuth simpleAuth;
+
+    @Subscribe
+    public void onPlayerChooseServer(PlayerChooseInitialServerEvent event) {
+        TPlayer tPlayer = PlayerManager.GET_TMP_PLAYER(event.getPlayer().getUsername());
+        simpleAuth.getLogger().info("PlayerChooseInitialServerEvent: " + tPlayer.getUsername() + " " + tPlayer.isNeedAuth() + " " + tPlayer.isLoggedIn());
+
+        if (tPlayer.isNeedAuth() && !tPlayer.isLoggedIn()) {
+            Optional<RegisteredServer> authServer = simpleAuth.getServersManager().getAuthServer();
+
+            if (authServer.isEmpty()) {
+                event.setInitialServer(null);
+                return;
+            }
+
+            tPlayer.setServer(authServer.get());
+            event.setInitialServer(authServer.get());
+        } else if (!tPlayer.isNeedAuth() && tPlayer.isLoggedIn()) {
+            Optional<RegisteredServer> lobbyServer = simpleAuth.getServersManager().getLobbyServer();
+
+            if (lobbyServer.isEmpty()) {
+                event.setInitialServer(null);
+                return;
+            }
+
+            event.setInitialServer(lobbyServer.get());
+        }
+    }
+
+    @Subscribe
+    public void onPreConnect(ServerPreConnectEvent event) {
+        TPlayer tPlayer = PlayerManager.GET_TMP_PLAYER(event.getPlayer().getUsername());
+        simpleAuth.getLogger().info("ServerPreConnectEvent: " + tPlayer.getUsername() + " " + tPlayer.isNeedAuth() + " " + tPlayer.isLoggedIn());
+
+        // Cancel server connection if player is not logged in
+        if (tPlayer.isNeedAuth() && !tPlayer.isLoggedIn()) {
+            Optional<RegisteredServer> server = event.getResult().getServer();
+
+            if (server.isEmpty()) {
+                event.setResult(ServerPreConnectEvent.ServerResult.denied());
+                return;
+            }
+
+            if (server.get().equals(tPlayer.getServer())) {
+                return;
+            }
+
+            event.setResult(ServerPreConnectEvent.ServerResult.denied());
+        }
+    }
+
+}
